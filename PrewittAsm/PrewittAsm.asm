e542771724ed0a72e789a837c8ea1b00e0ade209
JakubKrz <jk302230@student.polsl.pl>
2023-12-17 12:24:46 +0100

    Revert "Gradient_w_jedna_strone_w_ASM"
    This reverts commit 0a051dff756e67b45550e908c9d2bbd8726ffaec.

--- PrewittAsm/PrewittAsm.asm
+++ PrewittAsm/PrewittAsm.asm
@@ -14,114 +14,25 @@ applyPrewitt:
     imul    r8, 3         ; R8 = width * 3 (bo 3 bajty gbr)
 
     mov r12, r8
-    dec r12 ; do porowniania zeby pominac ostatni wiersz i klomne ( tu chyba ma byc odjac 3?)
+    dec r12 ; do porowniania zeby pominac ostatni wiersz i klomne
     mov r13, r9
     dec r13 ; -||-
 
-    mov r10, 1; zeby pominac 1 wiersz
+    xor     r10, r10       ; R10 = iterator wierszy (i)
+    inc r11 ; zeby pominac 1 wiersz
 
 outer_loop:
     ; Przygotowanie pêtli wewnêtrznej (iteracja po kolumnach)
     mov     rdi, rcx       ; RDI = wskaŸnik na obraz wejœciowy (przywrócenie pocz¹tkowego adresu)
     mov     rsi, rdx       ; RSI = wskaŸnik na obraz wyjœciowy (przywrócenie pocz¹tkowego adresu)
 
-    xor r11, r11     ; R13 = iterator kolumn (j)
-   ; zeby pominac 1 kolumne
+    xor     r11, r11       ; R13 = iterator kolumn (j)
+    inc r11 ; zeby pominac 1 kolumne
 
 inner_loop:
     ;Tutaj ma byc docelowo filtr prwitta
+    mov   byte PTR[rdi + r11 + 2], 255      ; ustawianie Red na 255
 
-    xorps xmm0, xmm0
-    xorps xmm1, xmm1
-
-    ;dodawnie 1 wartosc z filtru x (lewy gorny rog)
-    xor rax, rax
-    mov r14, r11 
-    sub r14, r8
-    sub r14, 3
-    add al, byte PTR[rsi + r14]
-    pinsrd xmm0, eax, 0
-    addps xmm1, xmm0
-
-    ;dodawnie 2 wartosc z filtru x (lewy srodkowy)
-    xorps xmm0, xmm0
-    xor rax, rax
-    mov r14, r11
-    sub r14, 3
-    add al, byte PTR[rsi + r14]
-    pinsrd xmm0, eax, 0
-    addps xmm1, xmm0
-
-    ;dodawnie 3 wartosc z filtru x (lewy dolny rog)
-    xorps xmm0, xmm0
-    xor rax, rax
-    mov r14, r11
-    add r14, r8
-    sub r14, 3
-    add al, byte PTR[rsi + r14]
-    pinsrd xmm0, eax, 0
-    addps xmm1, xmm0
-
-    ;odjecie 4 wartosc z filtru x (prawy gorny rog)
-    xorps xmm0, xmm0
-    xor rax, rax
-    mov r14, r11 
-    sub r14, r8
-    add r14, 3
-    add al, byte PTR[rsi + r14]
-    pinsrd xmm0, eax, 0
-    subps xmm1, xmm0
-
-    ;odjecie 5 wartosc z filtru x (prawy srodkowy)
-    xorps xmm0, xmm0
-    xor rax, rax
-    mov r14, r11
-    add r14, 3
-    add al, byte PTR[rsi + r14]
-    pinsrd xmm0, eax, 0
-    subps xmm1, xmm0
-
-    ;odjecie 6 wartosc z filtru x (prawy dolny rog)
-    xorps xmm0, xmm0
-    xor rax, rax
-    mov r14, r11
-    add r14, r8
-    add r14, 3
-    add al, byte PTR[rsi + r14]
-    pinsrd xmm0, eax, 0
-    subps xmm1, xmm0
-    
-    ;Trzeba dodac filt w kierunku y i 2 pixel naraz zeby 2 na raz przetwarzac i wykorzystac caly rejestr xmm
-    ;gradX (1) - gradY (1) - gradX (2) - gradY (2)
-    ;caly rejestr ^2
-    ;wyciagnac  dodac zrobic pierwiastek i zapisac
-    ;Dodac sprawdzenie czy wartosc nie przekrqacz 255???
-
-    ;tu dodac wartosc gradientx^2 + gradinty^2
-    ;na razei tylko grandient x
-    pmulld xmm1, xmm1   ; gradient * gradient
-    pextrd	eax, xmm1, 0    ; wyciagniecie wartosci do eax
-    ; tu powinno sie to dodac
-    
-    xorps xmm3, xmm3
-    cvtsi2sd    xmm3, eax ; z eax do smm3 zeby zrobi pierwiastek
-    sqrtsd xmm3, xmm3   ;pierwistek z gradienta
-    
-    xor rax, rax
-    cvtsd2si   eax, xmm3
-
-
-    mov byte PTR[rdi + r11 + 2], al ; red
-    mov byte PTR[rdi + r11 + 1], al ; green
-    mov byte PTR[rdi + r11 ], al  ;blue
-
-
-
-    ;test eax, eax     ; Test the sign of EAX
-    ;jns  skip_negate  ; Jump if not signed (negative)
-    ;neg  eax          ; Negate EAX if signed (negative)
-
-    ;skip_negate:
     ; Inkrementacja iteratora kolumn (j)
     add r11, 3
     cmp     r11, r12        ; sprawdzanie czy koniec wiersza (przedostatnia kolumna w wierszu)
