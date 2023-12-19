64bd7831ef8562aa66009837e16cd3fa3b05d18a
JakubKrz <jk302230@student.polsl.pl>
2023-12-19 22:14:46 +0100

    optymalizacja cpp

--- Prewitt/dllmain.cpp
+++ Prewitt/dllmain.cpp
@@ -21,35 +21,28 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 extern "C" __declspec(dllexport) void FiltrCpp(unsigned char* resultArray, unsigned char* byteArray, int width, int height)
 {
 
-    int horizontalFilter[3][3] = {
-        {-1, 0, 1},
-        {-1, 0, 1},
-        {-1, 0, 1}
-    };
-
-    int verticalFilter[3][3] = {
-        {-1, -1, -1},
-        { 0,  0,  0},
-        { 1,  1,  1}
-    };
-
     for (int y = 1; y < height - 1; ++y) {
         for (int x = 1; x < width - 1; ++x) {
-            for (int c = 0; c < 3; ++c) { 
-                int horizontalSum = 0;
-                int verticalSum = 0;
+            for (int c = 0; c < 3; ++c) {
+                int horizontalSum =
+                    - byteArray[((y - 1) * width + (x - 1)) * 3 + c]
+                    - byteArray[(y * width + (x - 1)) * 3 + c]
+                    - byteArray[((y + 1) * width + (x - 1)) * 3 + c]
+                    + byteArray[((y - 1) * width + (x + 1)) * 3 + c]
+                    + byteArray[(y * width + (x + 1)) * 3 + c]
+                    + byteArray[((y + 1) * width + (x + 1)) * 3 + c];
 
-                for (int i = -1; i <= 1; ++i) {
-                    for (int j = -1; j <= 1; ++j) {
-                        horizontalSum += byteArray[((y + i) * width + (x + j)) * 3 + c ] * horizontalFilter[i + 1][j + 1];
-                        verticalSum += byteArray[((y + i) * width + (x + j)) * 3 + c ] * verticalFilter[i + 1][j + 1];
-                    }
-                }
+                int verticalSum =
+                    - byteArray[((y - 1) * width + (x - 1)) * 3 + c]
+                    - byteArray[((y - 1) * width + x) * 3 + c]
+                    - byteArray[((y - 1) * width + (x + 1)) * 3 + c]
+                    + byteArray[((y + 1) * width + (x - 1)) * 3 + c]
+                    + byteArray[((y + 1) * width + x) * 3 + c]
+                    + byteArray[((y + 1) * width + (x + 1)) * 3 + c];
+
                 int gradient = static_cast<int>(std::sqrt(horizontalSum * horizontalSum + verticalSum * verticalSum));
 
-                resultArray[(y * width + x) * 3 + c] = static_cast<unsigned char>(std::sqrt(horizontalSum * horizontalSum + verticalSum * verticalSum));
-                //resultArray[(y * width + x) * 3 + 1] = static_cast<unsigned char>(std::sqrt(horizontalSum * horizontalSum + verticalSum * verticalSum));
-                //resultArray[(y * width + x) * 3 + 2] = static_cast<unsigned char>(std::sqrt(horizontalSum * horizontalSum + verticalSum * verticalSum));
+                resultArray[(y * width + x) * 3 + c] = static_cast<unsigned char>(gradient);
             }
         }
     }
