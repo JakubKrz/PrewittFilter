8e7a88ea55ea05dfa1a47644db8c69d4388de268
JakubKrz <jk302230@student.polsl.pl>
2023-12-19 13:39:28 +0100

    poprawienie kodu w asm

--- PrewittAsm/PrewittAsm.asm
+++ PrewittAsm/PrewittAsm.asm
@@ -10,7 +10,7 @@ applyPrewitt:
             
     ;r10 iteratot i
     ;r11 iterator j
-
+    push r15
     imul    r8, 3         ; R8 = width * 3 (bo 3 bajty gbr)
 
     mov r12, r8
@@ -32,119 +32,86 @@ outer_loop:
 inner_loop:
     ;Tutaj ma byc docelowo filtr prwitta
     
+    xor r15, r15
     xorps xmm0, xmm0 
     xorps xmm1, xmm1
 
     ;dodawnie 1 wartosc z filtru x (lewy gorny rog)
     xor rax, rax
-    mov r14, r11 
+    mov r14, r11 ;zamiast ciagle przenosic r11 do r14 uzyc inngeo rejestru?
     sub r14, r8
-    sub r14, 3
-    add al, byte PTR[rsi + r14]
-    pinsrd xmm0, eax, 0
-    addps xmm1, xmm0
-
+    sub r14, 3 
+    ;add al, byte PTR[rsi + r14]
+    mov r15b, byte PTR[rsi + r14]
+    add eax, r15d
     ;dodawnie 2 wartosc z filtru x (lewy srodkowy)
-    xorps xmm0, xmm0
-    xor rax, rax
-
-    add r14, r8
-    add al, byte PTR[rsi + r14]
-    pinsrd xmm0, eax, 0
-    addps xmm1, xmm0
-
+    add r14, r8                    ;przejscie do srodkowego
+    mov r15b, byte PTR[rsi + r14] 
+    add eax, r15d
     ;dodawnie 3 wartosc z filtru x (lewy dolny rog)
-    xorps xmm0, xmm0
-    xor rax, rax
-
     add r14, r8
-    add al, byte PTR[rsi + r14]
-    pinsrd xmm0, eax, 0
-    addps xmm1, xmm0
+    mov r15b, byte PTR[rsi + r14]
+    add eax, r15d
+    ;Zapisanie wyniku dodawania w xmm1 (0)
+    pinsrd xmm1, eax, 0 ; zmienic na mm?
 
-    ;odjecie 4 wartosc z filtru x (prawy gorny rog)
-    xorps xmm0, xmm0
+     ;odjecie 4 wartosc z filtru x (prawy gorny rog)
+    ;xorps xmm0, xmm0
     xor rax, rax
     mov r14, r11 
     sub r14, r8
-    add r14, 3
-    add al, byte PTR[rsi + r14]
-    pinsrd xmm0, eax, 0
-    subps xmm1, xmm0
-
+    add r14, 3 
+    mov r15b, byte PTR[rsi + r14]
+    add eax, r15d
     ;odjecie 5 wartosc z filtru x (prawy srodkowy)
-    xorps xmm0, xmm0
-    xor rax, rax
-    
     add r14, r8
-    add al, byte PTR[rsi + r14]
-    pinsrd xmm0, eax, 0
-    subps xmm1, xmm0
-
+    mov r15b, byte PTR[rsi + r14]
+    add eax, r15d
     ;odjecie 6 wartosc z filtru x (prawy dolny rog)
-    xorps xmm0, xmm0
-    xor rax, rax
-
     add r14, r8
-    add al, byte PTR[rsi + r14]
+    mov r15b, byte PTR[rsi + r14]
+    add eax, r15d
+    ;zapisanie wyniku do xmm1
     pinsrd xmm0, eax, 0
-    subps xmm1, xmm0
+
 
     ;dodawnie 1 wartosc z filtru y (lewy gorny rog)
-    xorps xmm0, xmm0 
     xor rax, rax
     mov r14, r11 
     sub r14, r8
     sub r14, 3
-    add al, byte PTR[rsi + r14]
-    pinsrd xmm0, eax, 1
-    addps xmm1, xmm0
-
+    mov r15b, byte PTR[rsi + r14]
+    add eax, r15d
     ;dodawnie 2 wartosc z filtru y (gorny srodkowy)
-    xorps xmm0, xmm0
-    xor rax, rax
-    
     add r14, 3
-    add al, byte PTR[rsi + r14]
-    pinsrd xmm0, eax, 1
-    addps xmm1, xmm0
-
+    mov r15b, byte PTR[rsi + r14]
+    add eax, r15d
     ;dodawnie 3 wartosc z filtru y (prawy gorny rog)
-    xorps xmm0, xmm0
-    xor rax, rax
-
     add r14, 3
-    add al, byte PTR[rsi + r14]
-    pinsrd xmm0, eax, 1
-    addps xmm1, xmm0
+    mov r15b, byte PTR[rsi + r14]
+    add eax, r15d
+    ;Zapisanie wyniku dodawania w xmm1 (1)
+    pinsrd xmm1, eax, 1
 
     ;odjecie 4 wartosc z filtru y (lewy dolny rog)
-    xorps xmm0, xmm0
     xor rax, rax
     mov r14, r11 
     add r14, r8
     sub r14, 3
-    add al, byte PTR[rsi + r14] ;to jest zbedne mozna po prostu zaladowac
-    pinsrd xmm0, eax, 1
-    subps xmm1, xmm0
-
+    mov r15b, byte PTR[rsi + r14]
+    add eax, r15d
     ;odjecie 5 wartosc z filtru y (dolny srodkowy)
-    xorps xmm0, xmm0
-    xor rax, rax
-
     add r14, 3
-    add al, byte PTR[rsi + r14]
-    pinsrd xmm0, eax, 1
-    subps xmm1, xmm0
-
+    mov r15b, byte PTR[rsi + r14]
+    add eax, r15d
     ;odjecie 6 wartosc z filtru y (prawy dolny rog)
-    xorps xmm0, xmm0
-    xor rax, rax
-
     add r14, 3
-    add al, byte PTR[rsi + r14]
+    mov r15b, byte PTR[rsi + r14]
+    add eax, r15d
+    ;Zapisanie do xmm0 (1)
     pinsrd xmm0, eax, 1
-    subps xmm1, xmm0
+
+    subps xmm1, xmm0 ;operacja x123-x456, y123-y456
     
     ;gradX (1) - gradY (1) - gradX (2) - gradY (2)
     ;Dodac sprawdzenie czy wartosc nie przekrqacz 255???
@@ -165,8 +132,9 @@ inner_loop:
 
     ; Inkrementacja iteratora kolumn (j)
     add r11, 1  ;zmienic na 3 jesli tylko 1 kolor
-    cmp     r11, r12        ; sprawdzanie czy koniec wiersza (przedostatnia kolumna w wierszu)
-    jl      inner_loop     ; 
+    ;inc rsi wtedy nie bedzie trzeba dodawac r11 przed kazdym adresowaniem 
+    cmp r11, r12        ; sprawdzanie czy koniec wiersza (przedostatnia kolumna w wierszu)
+    jl inner_loop     ; 
 
     ; Przesuniêcie wskaŸników na kolejny wiersz
     add     rcx, r8       ; wskaznik + width * 3
@@ -177,6 +145,7 @@ inner_loop:
     cmp     r10, r13        ; Sprawdzanie czy przedostatni wiersz koniec obrazu
     jl      outer_loop     ; 
 
+    pop r15
     ; Koniec
     ret
 
